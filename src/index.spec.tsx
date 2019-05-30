3d534dffb078856bb102ed33db9aa0b206883565
Hovhannes Babayan <bhovhannes@users.noreply.github.com>
2019-05-30 23:47:52 +0400

    test: upgrade to react-testing-library v8

--- src/index.spec.tsx
+++ src/index.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { render, fireEvent, cleanup } from 'react-testing-library'
+import { render, fireEvent, cleanup } from '@testing-library/react'
 import { useForceUpdate, useForceUpdateWithCallback } from './index'
 
 describe('useForceUpdate hook', function() {
