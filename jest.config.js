58823cecec6e4097209ec1d6030239cedd9e8f72
Hovhannes Babayan <bhovhannes@gmail.com>
2021-05-29 17:22:10 +0400

    chore: upgrade deps

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
+  testEnvironment: 'jsdom',
   coverageDirectory: './coverage',
   coverageReporters: ['lcov', 'html', 'text-summary'],
   collectCoverageFrom: ['./src/**/*.{ts,tsx}'],
